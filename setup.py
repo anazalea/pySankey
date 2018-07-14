ed620b30eaa0b97ded1741e0df93ed00833a0cbc
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2018-07-14 10:43:08 +0200

    Chore - create a requirements.txt and adapt setup.py
    We don't want to duplicate the requirements and we need a requirements.txt
    for other softwares than setup.py.

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as fh:
+    require = fh.readlines()
+require= [x.strip() for x in require]
+
 setuptools.setup(
     name="pySankey",
     version="1.0.0",
@@ -14,9 +18,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/anazalea/pySankey",
     packages=setuptools.find_packages(),
-    install_requires=['matplotlib>=2.1.0rc1',
-                      'seaborn>=0.8.1',
-                      'numpy>=1.13.3'],
+    install_requires=require,
     classifiers=(
         "Programming Language :: Python :: 3",
         'LICENSE :: OSI APPROVED :: GNU AFFERO GENERAL PUBLIC LICENSE V3',
